1b25ecdfead46e1225886f8ac3e0c665771c79e6
OSUBass <bassr@oregonstate.edu>
2021-05-19 14:33:00 -0500

    code written for function 3 (endian) in task.py.  test added to check conv_endian function for correct big endian implementation only at this point.

--- task.py
+++ task.py
@@ -9,8 +9,8 @@ def conv_endian(num, endian='big'):
         negative = True
         num = abs(num)
     exp = 1
-    before_conversion = []
-    after_conversion = []
+    before_conv = []
+    after_conv = []
     hex_letters = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
 
     # finds largest exponent of 16 that is less than the integer entered
@@ -21,54 +21,54 @@ def conv_endian(num, endian='big'):
     # loops until 16^exp gets to 16^0
     while exp > 0:
         remain = num//16**exp                   # variable for the whole number(no decimal) created from num/16^exp
-        before_conversion.append(remain)
+        before_conv.append(remain)
         leftover = remain * 16**exp
         num = num - leftover                    # creates new number for variable remain calculation
         exp -= 1
-    before_conversion.append(num)               # last number added to conversion list when exp = 0
+    before_conv.append(num)               # last number added to conversion list when exp = 0
 
     # converts digits over 9 to hex letters
-    for num in before_conversion:
+    for num in before_conv:
         if num > 9:
-            after_conversion.append(hex_letters[num])
+            after_conv.append(hex_letters[num])
         else:
-            after_conversion.append(num)
+            after_conv.append(num)
 
-    length = (len(after_conversion))
+    length = (len(after_conv))
     count_big = 0
     count_little = length - 1
 
     # formats return string. adds "0" to front of odd numbers and spacing every 2 characters
-    if len(after_conversion) % 2 == 0:
+    if len(after_conv) % 2 == 0:
         if endian == 'big':
-            final_num = str(after_conversion[count_big]) + str(after_conversion[count_big + 1]) + " "
+            final = str(after_conv[count_big]) + str(after_conv[count_big + 1]) + " "
             count_big += 2
             while count_big < length:
-                final_num = final_num + str(after_conversion[count_big]) + str(after_conversion[count_big + 1]) + " "
+                final = final + str(after_conv[count_big]) + str(after_conv[count_big + 1]) + " "
                 count_big += 2
         else:  # for little endian
-            final_num = str(after_conversion[count_little - 1]) + str(after_conversion[count_little]) + " "
+            final = str(after_conv[count_little - 1]) + str(after_conv[count_little]) + " "
             count_little -= 2
             while count_little > 0:
-                final_num = final_num + str(after_conversion[count_little - 1]) + str(after_conversion[count_little])
-                final_num = final_num + " "
+                final = final + str(after_conv[count_little - 1]) + str(after_conv[count_little])
+                final = final + " "
                 count_little -= 2
     else:
         if endian == 'big':
-            final_num = "0" + str(after_conversion[count_big]) + " "
+            final = "0" + str(after_conv[count_big]) + " "
             count_big += 1
             while count_big < length-1:
-                final_num = final_num + str(after_conversion[count_big]) + str(after_conversion[count_big + 1]) + " "
+                final = final + str(after_conv[count_big]) + str(after_conv[count_big + 1]) + " "
                 count_big += 2
         else:  # for little endian
-            final_num = ''
+            final = ''
             while count_little > 0:
-                final_num = final_num + str(after_conversion[count_little - 1]) + str(after_conversion[count_little]) + " "
+                final = final + str(after_conv[count_little - 1]) + str(after_conv[count_little]) + " "
                 count_little -= 2
                 if count_little == 0:
-                    final_num = final_num + "0" + str(after_conversion[count_little])
+                    final = final + "0" + str(after_conv[count_little])
 
     if negative is True:
-        final_num = "-" + final_num
+        final = "-" + final
 
-    return final_num
+    return final
