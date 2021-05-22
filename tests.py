f6db2fe3880db4d369d18a274c73a0ec786f1d88
Brenna Chenault <chenaulb@flip1.engr.oregonstate.edu>
2021-05-21 22:57:24 -0700

    added my_datetime and associated tests

--- tests.py
+++ tests.py
@@ -1,9 +1,46 @@
 import unittest
 from task import conv_endian
+from task import my_datetime
 import random
 
 
 class TestCase(unittest.TestCase):
+
+    def test1_function2(self):
+        expected = '01-01-1970'
+        self.assertEqual(expected, my_datetime(0))
+
+    def test2_function2(self):
+        expected = '02-01-1970'
+        self.assertEqual(expected, my_datetime(2721600))
+
+    def test3_function2(self):
+        expected = '01-01-1971'
+        self.assertEqual(expected, my_datetime(31536000))
+
+    def test4_function2(self):
+        expected = '02-29-1972'
+        self.assertEqual(expected, my_datetime(68212800))
+
+    def test5_function2(self):
+        expected = '03-01-1972'
+        self.assertEqual(expected, my_datetime(68256000))
+
+    def test6_function2(self):
+        expected = '11-29-1973'
+        self.assertEqual(expected, my_datetime(123456789))
+
+    def test8_function2(self):
+        expected = '03-01-2100'
+        self.assertEqual(expected, my_datetime(4107542400))
+
+    def test9_function2(self):
+        expected = '12-22-2282'
+        self.assertEqual(expected, my_datetime(9876543210))
+
+    def test10_function2(self):
+        expected = '02-29-8360'
+        self.assertEqual(expected, my_datetime(201653971200))
 
     def test1(self):
         self.assertTrue(True)
